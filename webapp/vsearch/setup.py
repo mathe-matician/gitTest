2b221ae22a1f3fa9e803ef0744117468d070018d
mathe-matician <mathezach@gmail.com>
2018-12-04 23:12:55 -0600

    vsearch file

---
+++ webapp/vsearch/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = "vsearch",
+    version = "1.0",
+    description = "searching baby",
+    author = "Me",
+    author_email = "me@me.com",
+    url = "me.com",
+    py_modules = ["vsearch"]
+    )
